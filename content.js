d6b59a1d51a802969d9b7969568e26f2d66ecb2d
hussainm <hussain@HM-Mac.local>
2014-06-09 23:07:54 +0500

    Added if statement to only replace youtube
    If statement checks if the src / href actually contains youtube.com.
    Saves time on expensive regex by using indexOf

--- content.js
+++ content.js
@@ -2,19 +2,26 @@
 var iframes = document.getElementsByTagName("iframe"); //array
 var regex = /youtube\.com\/(embed|v|watch)\/([\d\w]+)(.*)/i;
 for (var i=0,imax=iframes.length; i<imax; i++) {
-   iframes[i].src = iframes[i].src.replace(regex,"playit\.pk/embed\?v=$2");
+  if(iframes[i].src.indexOf("youtube.com/") !== -1 ){
+  	iframes[i].src = iframes[i].src.replace(regex,"playit\.pk/embed\?v=$2");
+  }	 
 }
 
+// embeds
 var embeds = document.getElementsByTagName("embed"); //array
 for (var i=0,imax=embeds.length; i<imax; i++) {
-   embeds[i].src = embeds[i].src.replace(regex,"playit\.pk/embed\?v=$2");
+	if(embeds[i].src.indexOf("youtube.com/") !== -1 ){
+  	embeds[i].src = embeds[i].src.replace(regex,"playit\.pk/embed\?v=$2");
+  } 	
 }
 
-// Links
+// links
 var links = document.getElementsByTagName("a"); //array
 var regexL = /youtube\.com\/(watch|embed|v)\?v=(.+)$/i;
 for (var i=0,imax=links.length; i<imax; i++) {
-   links[i].href = links[i].href.replace(regexL,"playit\.pk/$1\?v=$2");
+	if(links[i].src.indexOf("youtube.com/") !== -1 ){
+  	links[i].href = links[i].href.replace(regexL,"playit\.pk/$1\?v=$2");
+  }
 }
 
 console.log("All Youtube links have now been replaced with Playit.pk :D");
